917232ece1b73298c77c173246cdf4536414d6ed
luutrucuatoi <118989102+luutrucuatoi@users.noreply.github.com>
2022-12-28 09:33:21 +0700

    Update winlite10.sh

--- winlite10.sh
+++ winlite10.sh
@@ -1,38 +1,21 @@
+echo "Đang tải file xuống - Download windows files"
+wget -O lite10.qcow2 https://bit.ly/32OIl8Y
+echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp 5900 &>/dev/null &
-echo Please wait for installing...
-sudo apt update -y > /dev/null 2>&1
-echo "Installing QEMU (2-3m)..."
-sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
-echo Downloading Windows Disk...
-curl -L -o lite10.qcow2 https://bit.ly/32OIl8Y
-echo "Windows 10 x64 Lite On Gitpod, GCloud and GColab"
-echo Your VNC IP Address:
+nohup ./ngrok tcp 3388 &>/dev/null &
+echo Downloading File From toigamo.blogspot.com
+apt-get install qemu
+echo "Wait"
+echo "Starting Windows"
+echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "Note: Use Right-Click Or Ctrl+C To Copy"
-echo "Please Keep this Tab to Open, Maximum Time 50h"
-echo "Support YT Channel-> Aank is ME"
-echo "LinK: https://aank.me/Youtube"
-b='\033[1m'
-r='\E[31m'
-g='\E[32m'
-c='\E[36m'
-endc='\E[0m'
-enda='\033[0m'
-# Branding
-
-printf """$c$b
- 
-██╗     ██╗███╗   ██╗ ██████╗  ██████╗  █████╗ ██╗  ██╗ ██████╗ ███████╗████████╗██╗███╗   ██╗ ██████╗ 
-██║     ██║████╗  ██║██╔════╝ ██╔════╝ ██╔══██╗██║  ██║██╔═══██╗██╔════╝╚══██╔══╝██║████╗  ██║██╔════╝ 
-██║     ██║██╔██╗ ██║██║  ███╗██║  ███╗███████║███████║██║   ██║███████╗   ██║   ██║██╔██╗ ██║██║  ███╗
-██║     ██║██║╚██╗██║██║   ██║██║   ██║██╔══██║██╔══██║██║   ██║╚════██║   ██║   ██║██║╚██╗██║██║   ██║
-███████╗██║██║ ╚████║╚██████╔╝╚██████╔╝██║  ██║██║  ██║╚██████╔╝███████║   ██║   ██║██║ ╚████║╚██████╔╝
-╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝
-    $r  Support YT Channel-> Aank is ME © 2022 $c https://aank.me/Youtube 
-          
-$endc$enda""";
-sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+echo "Ctrl+C để copy - Ctrl+C To Copy"
+echo Username: akuh
+echo Password: Akuh.Net
+echo "Chờ 1-2 phút để hoàn tất - Wait 1-2 minute to finish bot"
+echo "Vui lòng không tắt cửa sổ này - Dont Close This Tab"
+echo "Please support toigamo.blogspot.com thank you"
+qemu-system-x86_64 -hda w7x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
