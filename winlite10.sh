81720e29210b364ccdb50d7d5894bb916515bdf7
luutrucuatoi <118989102+luutrucuatoi@users.noreply.github.com>
2023-01-03 12:51:49 +0700

    Update winlite10.sh

--- winlite10.sh
+++ winlite10.sh
@@ -1,21 +1,20 @@
-echo "Đang tải file xuống - Download windows files"
-wget -O w7x64.img https://bit.ly/32OIl8Y
-echo "Download ngrok"
+echo "Tải xuống các tệp windows"
+wget -O w10x64.img https://bit.ly/akuhnetW10x64
+echo "Tải ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Ctrl + V Authtoken: " CRP 
+read -p "Ctrl + V Authtoken(nhập token ngrok): " CRP 
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 3388 &>/dev/null &
-echo Downloading File From toigamo.blogspot.com
+echo Tải xuống tệp từ sever
 apt-get install qemu
-echo "Wait"
-echo "Starting Windows"
-echo RDP Address:
+echo "Đợi"
+echo "Khởi động Windows"
+echo RDP Địa chỉ:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "Ctrl+C để copy - Ctrl+C To Copy"
-echo Username: akuh
-echo Password: Akuh.Net
-echo "Chờ 1-2 phút để hoàn tất - Wait 1-2 minute to finish bot"
-echo "Vui lòng không tắt cửa sổ này - Dont Close This Tab"
-echo "Please support toigamo.blogspot.com thank you"
-qemu-system-x86_64 -hda w7x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
+echo "Ctrl+C để Copy"
+echo "Đợi 1-2 Phút để hoàn thành thiết lập"
+echo "Không đóng tab này"
+echo "hãy ủng hộ Hoàng vtmc xin cảm ơn"
+qemu-system-x86_64 -hda w10x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
+sleep 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
