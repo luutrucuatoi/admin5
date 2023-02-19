768d85a12e18ea2de35bb8fdc6e3232130bc4c7d
luutrucuatoi <118989102+luutrucuatoi@users.noreply.github.com>
2023-02-19 21:33:56 +0700

    Update w7.sh

--- w7.sh
+++ w7.sh
@@ -1,4 +1,4 @@
-ngrok tcp 5900 -region ap &>/dev/null
+nohup ngrok tcp 5900 -region ap &>/dev/null
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 4096M -machine usb=on -device usb-tablet > /dev/null 2>&1
