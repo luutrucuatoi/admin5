e4a007c9d97c7c0c0848fab5e60a0da5a19e5aa8
luutrucuatoi <118989102+luutrucuatoi@users.noreply.github.com>
2023-02-20 05:44:16 +0700

    Add files via upload

---
+++ w7.sh
@@ -0,0 +1,11 @@
+./ngrok tcp 20 -region ap &>/dev/null &
+sleep 2
+echo ================================
+echo                                 
+echo                                 
+echo Your VNC IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo                                 
+echo                                 
+echo ================================
+qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=1  -m 1024M -machine usb=on -device usb-tablet > /dev/null 2>&1
