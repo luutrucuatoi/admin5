56cb5f0c9b0326bd6c540f40be53a740af4fb24d
luutrucuatoi <118989102+luutrucuatoi@users.noreply.github.com>
2023-02-20 05:54:08 +0700

    Rename 7.sh to w7.sh

--- w7.sh
+++ w7.sh
@@ -1,5 +1,6 @@
 ./ngrok tcp 20 -region ap &>/dev/null &
 sleep 2
+qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=1  -m 1024M -machine usb=on -device usb-tablet &>/dev/null &
 echo ================================
 echo                                 
 echo                                 
@@ -8,4 +9,3 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo                                 
 echo                                 
 echo ================================
-qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=1  -m 1024M -machine usb=on -device usb-tablet > /dev/null 2>&1
