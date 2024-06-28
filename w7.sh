9ad2d03129345ef24d23bd9d72118c00cdd5a0e7
luutrucuatoi <118989102+luutrucuatoi@users.noreply.github.com>
2024-06-28 20:29:31 +0700

    Update w7.sh

--- w7.sh
+++ w7.sh
@@ -1,7 +1,12 @@
 echo "Tải xuống các tệp windows"
 wget -O w10x64.img https://bit.ly/akuhnetW10x64
 echo "Tải ngrok"
-sudo tar -xvzf ~/Downloads/https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok
 read -p "Ctrl + V Authtoken(nhập token ngrok): " CRP 
 ngrok authtoken $CRP 
 nohup ngrok tcp 3388 &>/dev/null &
